1946735e797b46b9a3c07f7a8b3700521ee68cd4
Bibek Shrestha <bibekshrestha@mac.lan>
2025-01-18 22:40:03 -0500

    Added db service at dockerfile Postgresql database

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator function
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def substract(x, y):
+    """Substract x and y return result."""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class ClacTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        """Test substracting numbers."""
+        res = calc.substract(4,3)
+
+        self.assertEqual(res, 1)
